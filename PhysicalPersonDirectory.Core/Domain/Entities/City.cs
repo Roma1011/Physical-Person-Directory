d789bcfccc510760aeadb62c7ac3167e68a27e55
Roma1011 <romski48@gmail.com>
2025-02-27 12:16:54 +0400

    refactor: update City class to inherit from Entity and include Person property

--- PhysicalPersonDirectory.Core/Domain/Entities/City.cs
+++ PhysicalPersonDirectory.Core/Domain/Entities/City.cs
@@ -1,6 +1,10 @@
-namespace PhysicalPersonDirectory.Core.Entities;
+using PhysicalPersonDirectory.Core.Domain.Entities.PersonEntity;
+using PhysicalPersonDirectory.Infra.Abstraction.Common;
 
-internal class City
+namespace PhysicalPersonDirectory.Core.Domain.Entities;
+
+internal class City:Entity
 {
-    
+    public string Name { get; internal set; }=null!;
+    public Person Person { get;}
 }
