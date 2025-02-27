6a6541ec6ae56c3793036ad6329c603584b4e986
Roma1011 <romski48@gmail.com>
2025-02-27 14:43:38 +0400

    Add initial migration for Physical Person context with City and Person tables

---
+++ PhysicalPersonDirectory.Core/DAO/Context/Migrations/20250227104055_Initial_Physical_Person_Context.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PhysicalPersonDirectory.Core.DAO.Context.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial_Physical_Person_Context : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "City",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_City", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Pid = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    TypeOfPhone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                    ImageSource = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Gender = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    DateOfPBirth = table.Column<DateOnly>(type: "date", nullable: false),
+                    CityId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.Id)
+                        .Annotation("SqlServer:Clustered", true);
+                    table.ForeignKey(
+                        name: "FK_Person_City_CityId",
+                        column: x => x.CityId,
+                        principalTable: "City",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RelatedPersons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PersonId = table.Column<int>(type: "int", nullable: false),
+                    RelatedPersonId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RelatedPersons", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RelatedPersons_Person_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "Person",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_RelatedPersons_Person_RelatedPersonId",
+                        column: x => x.RelatedPersonId,
+                        principalTable: "Person",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Person_CityId",
+                table: "Person",
+                column: "CityId",
+                unique: true,
+                filter: "[CityId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Person_Pid",
+                table: "Person",
+                column: "Pid",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RelatedPersons_PersonId",
+                table: "RelatedPersons",
+                column: "PersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RelatedPersons_RelatedPersonId",
+                table: "RelatedPersons",
+                column: "RelatedPersonId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RelatedPersons");
+
+            migrationBuilder.DropTable(
+                name: "Person");
+
+            migrationBuilder.DropTable(
+                name: "City");
+        }
+    }
+}
