eca5c6a367a6ef7ba96f85ffce46fb59de0365c3
Roma1011 <romski48@gmail.com>
2025-02-27 14:39:24 +0400

    Rename PersonRelation to RelatedPerson and update properties for clarity; add RelatedPersonConfiguration for EF Core setup

---
+++ PhysicalPersonDirectory.Core/DAO/Context/SetConfigurations/RelatedPersonConfiguration.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PhysicalPersonDirectory.Core.Domain.Entities;
+
+namespace PhysicalPersonDirectory.Core.DAO.Context.SetConfigurations;
+
+internal class RelatedPersonConfiguration:IEntityTypeConfiguration<RelatedPerson>
+{
+    public void Configure(EntityTypeBuilder<RelatedPerson> builder)
+    {
+//-----------------------------------Table Config----------------------------------------------------------------------------------
+        builder.HasKey(rl => rl.Id);
+        
+//-----------------------------------Relationship Config----------------------------------------------------------------------------------
+        builder
+            .HasOne(pr => pr.Person)
+            .WithMany(p => p.PersonRelations)
+            .HasForeignKey("PersonId")
+            .OnDelete(DeleteBehavior.Restrict);
+        
+        builder
+            .HasOne(pr => pr.Related)
+            .WithMany() 
+            .HasForeignKey("RelatedPersonId")
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
