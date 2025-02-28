9afb449cc0398a92012c1f1e0633eedd4844ac71
Roma1011 <romski48@gmail.com>
2025-02-28 15:33:50 +0400

    Implement file storage and update person service to handle photo uploads

--- PhysicalPersonDirectory.Core/UseCases/Services/Promises/IPersonService.cs
+++ PhysicalPersonDirectory.Core/UseCases/Services/Promises/IPersonService.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http;
 using PhysicalPersonDirectory.Core.UseCases.DTOs.Request;
 using PhysicalPersonDirectory.Infra.Abstraction.Common;
 
@@ -5,5 +6,6 @@ namespace PhysicalPersonDirectory.Core.UseCases.Services.Promises;
 
 public interface IPersonService
 {
-    public Task<Result> AddPersonAsync(AddPerson person);
+    public Task<Result<bool>> AddPersonAsync(AddPerson addRequest);
+    public Task<Result<bool>> AppendPhotoAsync(AppendImage appendImage);
 }
